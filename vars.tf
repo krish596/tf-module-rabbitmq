fa19d07fe5115c3dc38bed15f7584a30cb9821c3
Chimtamreddy <kr7348202@gmail.com>
2024-08-18 14:55:05 +0530

    Terraform

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "tags" {}
+variable "env" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "ssh_ingress_cidr" {}
+variable "instance_type" {}
+
+
+
+
